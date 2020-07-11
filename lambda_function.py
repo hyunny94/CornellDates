2025ed4825f25a21678727a6aa9ad8f5b6528dae
hyunny94 <hj283@cornell.edu>
2020-07-11 13:42:58 -0400

    WhenIsEvent intent working

--- lambda_function.py
+++ lambda_function.py
@@ -18,8 +18,23 @@ import boto3
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key
 
+from utils import get_default_academic_year, pick_closest_event_for_grade
+
+import time
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+DEFAULT_ACADEMIC_YEAR = get_default_academic_year()
+DEFAULT_LOCATION = "ithaca"
+DEFAULT_GRADE = "freshman"
+DYNAMODB_TABLE_NAME = "CornellDates" + DEFAULT_ACADEMIC_YEAR
+SSML_START = "<speak>"
+SSML_EMPHASIS_STRONG_START = '<emphasis level="strong">'
+SSML_EMPHASIS_STRONG_END = '</emphasis>'
+SSML_EMPHASIS_MODERATE_START = '<emphasis level="moderate">'
+SSML_EMPHASIS_MODERATE_END = '</emphasis>'
+SSML_END = "</speak>"
 
 
 class LaunchRequestHandler(AbstractRequestHandler):
@@ -59,40 +74,91 @@ class HelloWorldIntentHandler(AbstractRequestHandler):
         )
 
 
-class WhenIsAnEventIntentHandler(AbstractRequestHandler):
-    """Handler for WhenIsAnEvent Intent."""
+class WhenIsEventIntentHandler(AbstractRequestHandler):
+    """Handler for WhenIsEvent Intent."""
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return ask_utils.is_intent_name("WhenIsAnEvent")(handler_input)
+        return ask_utils.is_intent_name("WhenIsEvent")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         
         # 1. Retrieve slots from alexa
         slots = handler_input.request_envelope.request.intent.slots
-        event = slots["event"].value
+        if slots['DeadlineEvent'].resolutions:
+            if slots['DeadlineEvent'].resolutions.resolutions_per_authority[0].values:
+                event_abb_name = slots["DeadlineEvent"].resolutions.resolutions_per_authority[0].values[0].value.name
+            else:
+                raise Exception("sorry we have the event on record but can you try rephrasing your question?")
+        elif slots['PeriodEvent'].resolutions:
+            if slots['PeriodEvent'].resolutions.resolutions_per_authority[0].values:
+                event_abb_name = slots["PeriodEvent"].resolutions.resolutions_per_authority[0].values[0].value.name
+            else:
+                raise Exception("sorry we have the event on record but can you try rephrasing your question?")
+        elif slots['ReleaseEvent'].resolutions:
+            if slots['ReleaseEvent'].resolutions.resolutions_per_authority[0].values:
+                event_abb_name = slots["ReleaseEvent"].resolutions.resolutions_per_authority[0].values[0].value.name
+            else:
+                raise Exception("sorry we have the event on record but can you try rephrasing your question?")
+        else:
+            raise Exception("sorry we cannot answer the question at this time.")
 
         # 2. Query the dynamoDB
-        start = "hello.."
         try:
             client = boto3.client('dynamodb')
             response = client.query(
                 ExpressionAttributeValues={
                     ':v1': {
-                        'S': event,
+                        'S': event_abb_name,
                     },
                 },
-                KeyConditionExpression="event = :v1",
-                TableName='CornellDates',
+                KeyConditionExpression="event_abb_name = :v1",
+                TableName=DYNAMODB_TABLE_NAME,
             )
             print(response)
         except ClientError as e:
             print(e.response['Error']['Message'])
         else:
-            start = response['Items'][0]['start']['N']
+            event = pick_closest_event_for_grade(response['Items'], DEFAULT_GRADE)
 
         # 3. format the answer
-        speak_output = event + " is " + start
+        date_granularity = event['date_granularity']['S']
+        internal_category = event['internal_category']['S']
+        start = int(event['start']['N'])
+        speak_output = SSML_START
+        speak_output += event_abb_name
+        # depending on 
+        # 3.1 this event's ['date_granularity'] => one of ["date", "hour"]
+        if internal_category == 'period':
+            end = int(event['end']['N'])
+            if date_granularity == "date":
+                start = time.strftime('%Y-%m-%d', time.localtime(start))
+                end = time.strftime('%Y-%m-%d', time.localtime(end))
+            elif date_granularity == "hour": 
+                start = time.strftime('%Y-%m-%d %H:%M', time.localtime(start))
+                end = time.strftime('%Y-%m-%d %H:%M', time.localtime(end))
+        elif internal_category == 'release':
+            if date_granularity == "date":
+                start = time.strftime('%Y-%m-%d', time.localtime(start))
+            elif date_granularity == "hour": 
+                start = time.strftime('%Y-%m-%d %H:%M', time.localtime(start))
+        else: # internal_category == 'deadline'
+            if date_granularity == "date":
+                start = time.strftime('%Y-%m-%d', time.localtime(start))
+            elif date_granularity == "hour": 
+                start = time.strftime('%Y-%m-%d %H:%M', time.localtime(start))
+        
+        # 3.2 this event's ['internal_category'] => "period" means the event has "start" and "end"
+        if internal_category == 'period':
+            speak_output += " is from " +\
+                 SSML_EMPHASIS_MODERATE_START + start + SSML_EMPHASIS_MODERATE_END +\
+                      " until " +\
+                           SSML_EMPHASIS_MODERATE_START + end + SSML_EMPHASIS_MODERATE_END
+        else:
+            speak_output += " is on " +\
+                SSML_EMPHASIS_MODERATE_START + start + SSML_EMPHASIS_MODERATE_END
+
+        speak_output += SSML_END
 
         # 4. respond
         return (
@@ -209,7 +275,7 @@ sb.add_request_handler(HelloWorldIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
-sb.add_request_handler(WhenIsAnEventIntentHandler())
+sb.add_request_handler(WhenIsEventIntentHandler())
 sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
 
 
--- read_csv_and_import_to_dynamodb.py
+++ read_csv_and_import_to_dynamodb.py
@@ -1,38 +1,90 @@
 import csv
 import boto3
+from utils import get_default_academic_year
+import datetime
 
 def read_csv_into_dictlst():
-    with open('CornellDatesTest - Events.csv') as f:
+    with open('CornellDates - Events.csv') as f:
         a = [{k: v for k, v in row.items()}
             for row in csv.DictReader(f, skipinitialspace=True)]
-        print(a)
         return a
 
 def import_to_dynamodb():
     dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 
-    table = dynamodb.Table('CornellDates')
+    table = dynamodb.Table('CornellDates' + get_default_academic_year())
 
     dict_lst = read_csv_into_dictlst()
 
     with table.batch_writer() as batch:
         for item in dict_lst:
+    
+            # Add non-nullable fields
+            dict = {
+                'event_org_name': item['event_org_name'],
+                'category': item['category'],
+                'semester': item['semester'],
+                'academic_year': item['academic_year'],
+                'event_abb_name': item['event_abb_name'],
+                'internal_category': item['internal_category'],
+                'grade': item['grade'].split(','),
+                'date_granularity': item['date_granularity']
+            } 
+
+            # Handle nullable fields
+            start = item['start']
+            end = item['end']
+            event_verb = item['event_verb']
+            # start is sort-key thus required.
+            if start != '':
+                dict['start'] = convert_to_epoch_time(start)
+                if end != '':
+                    dict['end'] = convert_to_epoch_time(end)
+            else:
+                dict['start'] = convert_to_epoch_time(end)
+            
+            if event_verb != '':
+                dict['event_verb'] = event_verb
+                
             batch.put_item(
-                Item={
-                    'category': item['category'],
-                    'subcategory': item['subcategory'],
-                    'event': item['event'],
-                    'event_abb_name': item['event_abb_name'],
-                    'event_verb': item['event_verb'],
-                    'semester': item['semester'],
-                    'academic_year': item['academic_year'],
-                    'grade': item['grade'].split(','),
-                    'location': item['location'].split(','),
-                    'start': int(item['start']),
-                    'end': int(item['end']),
-                    'date_granularity': item['date_granularity']
-                }
+                Item = dict
             )
+
+'''
+time is a string in the following format
+"2020,8,10,23,59" 
+returns 1597118340
+'''
+def convert_to_epoch_time(time):
+    times = time.split(',')
+    year = int(times[0])
+    month = int(times[1])
+    date = int(times[2])
+    hour = int(times[3])
+    minute = int(times[4])
+    epoch_time = int(datetime.datetime(year, month, date, hour, minute).timestamp())
+    return epoch_time
+
+def update_dynamodb():
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+    table = dynamodb.Table('CornellDates' + get_default_academic_year())
+    dict_lst = read_csv_into_dictlst()
+
+    with table.batch_writer() as batch:
+        for item in dict_lst:
+            event_abb_name = item['event_abb_name']
+            start = item['start'] if item['start'] != '' else item['end']
+            start = convert_to_epoch_time(start)
+            
+            batch.update_item(
+                Key = {
+                    'event_abb_name': event_abb_name,
+                    'start': start
+                },
+                UpdateExpression='SET end = :val1',
+                ExpressionAttributeValues={
+                    ':val1': "CHANGE ME~~~~"
+                })
 
 if __name__ == "__main__":
     import_to_dynamodb()
